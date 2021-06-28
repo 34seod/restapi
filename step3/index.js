08399602a479f918a272e133ef6ae2179792c673
btpink-seo <btpink.seo@gmail.com>
2021-06-28 21:27:38 +0900

    add public

--- step3/index.js
+++ step3/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
-  res.send('hello world');
+  res.sendFile('index.html');
 });
 
 app.listen(3001);
